1b68e4dbfd98b1ad670422e7b320aec052230d6b
Emily Sellers <emilymelindasellers@gmail.com>
2023-01-18 11:41:58 -0800

    .filter alternate solution comments

--- filter.js
+++ filter.js
@@ -2,9 +2,14 @@
 // OUTPUT: only countries in Oceania
 // REQS: use .filter
 
+// **ALTERNATES - INCLUDES, DESTRUCTURED**
+// export const filterOceania = (countries) =>
+//   countries.filter(({ continent }) => continent?.includes('Oceania'));
+
 // export const filterOceania = (countries) => {
 //   return countries.filter((continent) => country.continent === 'Oceania');
 // };
+
 export const filterOceania = (countries) =>
   countries.filter(({ continent }) => continent === 'Oceania');
 
@@ -12,9 +17,11 @@ export const filterOceania = (countries) =>
 // OUTPUT: only countries whose name that include the letters 'au'
 // REQS: use .filter
 
+// **NOT DESTRUCTURED and EXPLICIT RETURN**
 // export const filterAu = (countries) => {
 //   return countries.filter((country) => country.name.includes('au'));
 // };
+
 // **DESTRUCTURED and IMPLICIT RETURN**
 export const filterAu = (countries) => countries.filter(({ name }) => name.includes('au'));
 
@@ -29,6 +36,11 @@ export const filterYoungDogs = (dogs) => {
 // OUTPUT: a list of names of dogs that are 2 years old or younger
 // REQS: use .filter AND .map OR use your filterYoungDogs function and a map
 
+// **ALTERNATE**
+// export const getYoungDogNames = (dogs) => {
+//   const filteredDogs = filterYoungDogs(dogs).map((dog) => dog.name);
+//   return filteredDogs;
+// };
 export const getYoungDogNames = (dogs) => {
   return dogs.filter(({ age }) => age <= 2).map(({ name }) => name);
 };
