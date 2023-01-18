ecef44fcef43e2875cb5e955dd52aa194a276e29
Emily Sellers <emilymelindasellers@gmail.com>
2023-01-18 09:58:36 -0800

    .map getCountryNames and getCountryCodes tests passing

--- map.js
+++ map.js
@@ -2,13 +2,21 @@
 // OUTPUT: an array of the names
 // REQS: use .map
 
-export const getCountryNames = (countries) => {};
+// export const getCountryNames = (countries) => {
+//     const newCountries = countries.map((country) => country.name);
+//     return newCountries;
+// };
+
+export const getCountryNames = (countries) => countries.map((country) => country.name);
+
+//   countries.map((country) => return country.name);
 
 // INPUT: the array of countries from data.js
 // OUTPUT: an array of the names and their iso2 country codes using .map
 // REQS: use .map
 
-export const getCountryCodes = (countries) => {};
+export const getCountryCodes = (countries) =>
+  countries.map((country) => [country.name, country.iso2]);
 
 // INPUT: the array of shoes from data.js
 // OUTPUT: an array of the brands
